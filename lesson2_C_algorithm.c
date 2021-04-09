6960cedd14c9b21b45c5452e62e8e61ae45c9da6
Mike <ilmivy1024@gmail.com>
2021-04-09 20:32:46 +0600

    first commit

---
+++ lesson2_C_algorithm.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+// 1. Реализовать функцию перевода из десятичной системы в двоичную, используя рекурсию.
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
